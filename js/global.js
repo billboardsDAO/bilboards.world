59af03c7e140535344cf45fb00a7ec0d5b6a1b0b
billboardsDAO <98717622+billboardsDAO@users.noreply.github.com>
2022-02-10 22:52:18 -0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-// object to insert global functions of the dapp
+// object to insert global window functions of the dapp
 var dapp = {};
 
 // localforage configuration
@@ -29,12 +29,70 @@ ons.ready(function () {
     
   ons.createElement('templates/connect.html', { append: true })
     .then(function (sheet) {
-      dapp.showConnection = sheet.show.bind(sheet);
-      dapp.hideConnection = sheet.hide.bind(sheet);
-    });
+      window.dapp.showConnection = sheet.show.bind(sheet);
+      window.dapp.hideConnection = sheet.hide.bind(sheet);
+    });   
     
 });
 
-setupContract();
+
+  window.dapp.open_menu = function() {
+    document.getElementById('menu').open();
+  };
+
+  window.dapp.open_search = function() {
+    document.getElementById('search').open();
+  };
+
+  window.dapp.load = function(page) {
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page).then(menu.close.bind(menu));
+  };
+
+  window.dapp.share = function(){
+
+      try {
+        navigator.share({
+          title: 'MDN',
+          text: 'Learn web development on MDN!',
+          url: 'https://developer.mozilla.org'
+        })
+      } catch(err) {}
+
+  }
+          
+document.addEventListener('init', function(event) {   
+
+    var page = event.target;
+
+    // Creating map options
+
+    // check element exists
+    if (page.querySelector('#map')) {                
+
+      var mapOptions = {
+        center: [17.385044, 78.486671],
+        zoom: 10
+      }
+
+      // Creating a map object
+      var map = new L.map('map', mapOptions);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+    };
+
+    if (page.querySelector('#share-btn')) {
+        if (!navigator.share) page.querySelector('#share-btn').style.display = "none";            
+    }
+
+});
+
+
+
+setupContract(); // METAMASK MODULE
 
 
