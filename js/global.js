05137b83941402076daf8877554f2c7b77e1b81b
billboardsDAO <98717622+billboardsDAO@users.noreply.github.com>
2022-04-03 20:11:31 -0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -3,7 +3,7 @@ var dapp = {};
 
 window.Buffer = buffer.Buffer;
 
-window.dapp.address = "Amhr6GDNoq57vkEtQ3AKowka3GiGYDbZMhWvNPpfkehV7hKPoezG";
+window.dapp.address = "AmgaYPNR8LWaaN4vortKDTktXAVmdvC3NJBWMkc1Geoyu5XGNyeS";
 
 window.dapp.contract = undefined;
 window.dapp.abi = undefined;
