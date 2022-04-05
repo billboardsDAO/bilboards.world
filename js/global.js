2f4ed751c34057a9a917a02e77e310087322a71a
billboardsDAO <98717622+billboardsDAO@users.noreply.github.com>
2022-04-04 21:25:25 -0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -277,7 +277,6 @@ ons.ready(function() {
     });    
            
     document.querySelector('#menu').addEventListener('preopen', function() {
-        alert("1");
 
         if (window.account) {
             document.getElementById("wallet-menu-connected").style.display = "block";
